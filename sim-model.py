7ee02348be6cd22976d9d2826c6769c72493b56e
Edward Reddick <ed.red55@gmail.com>
2021-04-09 15:16:20 -0300

    Pipeline functionality, sample_weight pipeline

--- sim-model.py
+++ sim-model.py
@@ -6,9 +6,8 @@ Playing with python, typical data science tasks, and functional programming.
 ## Step 0: Create fake small dataset
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import PoissonRegressor
-from sklearn.model_selection import cross_val_score, KFold
+from sklearn.model_selection import train_test_split, cross_val_score, KFold
 from sklearn.preprocessing import FunctionTransformer, StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
@@ -36,38 +35,33 @@ del(n, beta0, beta1, beta2, mu)
 ## Step 1: pre-process data
 ## naive pre-processing, this should occur within CV folds for any pre-processing
 ## that requires estimation to avoid leakage similar to recipes in R
-#def pre_pro(df):
-#    df = df.replace(cleanup_nums)
-#    return(df)
-
-#df_t = pre_pro(df)
-df_t = df
-y = df_t['count']
-X = df_t.loc[:, df_t.columns != 'count']
-del(df, df_t)
+y = df['count']
+sw = df['exposure']
+X = df.drop(['count', 'exposure'], axis=1)
+del(df)
 
 ## messy to create 4 datasets, look into python alternatives similar to 
 ## rsample::initial_split
-Xtrain, Xtest, ytrain, ytest = train_test_split(X, y)
-del(X, y)
+Xtrain, Xtest, ytrain, ytest, swtrain, swtest = train_test_split(
+    X, y, sw, test_size = 0.25, random_state = 5)
+
+del(X, y, sw)
 
 ## test out column imputation
 ## introduce NaN values
 Xtrain.loc[Xtrain.index[0], 'num_a'] = np.NaN
-Xtrain.loc[Xtrain.index[0], 'exposure'] = np.NaN
 Xtest.loc[Xtest.index[0], 'num_a'] = np.NaN
-Xtest.loc[Xtest.index[0], 'exposure'] = np.NaN
 
-imp = SimpleImputer(missing_values=np.nan, strategy='mean')
+
+#imp = SimpleImputer(missing_values=np.nan, strategy='mean')
 
 ## test out sci-kit learn custom preprocessing
-transformer_num = FunctionTransformer(func=imp
-                                  , validate=False)
+#transformer_num = FunctionTransformer(func=imp
+#                                  , validate=False)
 
 ## for catagorical variables 
 ## pool infrequently occurring values into an "other" category
 #Xtrain.loc[Xtrain.index[0], 'char_a'] = np.NaN
-
 def get_support_levels(x_char, threshold):
     df = pd.DataFrame({'x_char' :x_char})
     df = pd.DataFrame(df['x_char'].value_counts()/len(x_char))
@@ -82,42 +76,35 @@ def step_other(x_char, sup_lev):
     
 step_other(Xtrain['char_a'], sup_lev)
     
-class CustomScaler(BaseEstimator, TransformerMixin):
-    def __init__(self):
-        super().__init__()
-        self.means_ = None
-        self.std_ = None
+# class CustomScaler(BaseEstimator, TransformerMixin):
+#     def __init__(self):
+#         super().__init__()
+#         self.means_ = None
+#         self.std_ = None
 
-    def fit(self, X, y=None):
-        X = X.to_numpy()
-        self.means_ = X.mean(axis=0, keepdims=True)
-        self.std_ = X.std(axis=0, keepdims=True)
+#     def fit(self, X, y=None):
+#         X = X.to_numpy()
+#         self.means_ = X.mean(axis=0, keepdims=True)
+#         self.std_ = X.std(axis=0, keepdims=True)
 
-        return self
+#         return self
 
-    def transform(self, X, y=None):
-        X[:] = (X.to_numpy() - self.means_) / self.std_
+#     def transform(self, X, y=None):
+#         X[:] = (X.to_numpy() - self.means_) / self.std_
 
-        return X
+#         return X
 
-my_CustomScaler = CustomScaler()
-
-
-## this doesn't work probably because of first transformation being passed back
-## as array with column names, or more likely because it's using the orignal num_a
-## which has na values
-#ct = ColumnTransformer(
-#    [("num", imp, ['num_a', 'exposure']), 
-#     ("num1", my_CustomScaler, ['num_a'])])
+# my_CustomScaler = CustomScaler()
 
 # define transformers
 si_n = SimpleImputer(missing_values=np.nan, strategy='mean')
 si_c = SimpleImputer(strategy='constant', fill_value='other')
 ss = StandardScaler()
-ohe = OneHotEncoder()
+ohe = OneHotEncoder(drop='first')
+
 # define column groups with same processing
 cat_vars = ['char_a']
-num_vars = ['num_a', 'exposure']
+num_vars = ['num_a']
 # set up pipelines for each column group
 categorical_pipe = Pipeline([('si_c', si_c), ('ohe', ohe)])
 numeric_pipe = Pipeline([('si_n', si_n), ('ss', ss)])
@@ -130,18 +117,11 @@ ct = ColumnTransformer(
                     remainder='drop',
                     n_jobs=-1
                     )
-
-#ct = ColumnTransformer(
-#    [("num", imp, ['num_a', 'exposure']), 
-#     ("char", transformer_char, ['char_a'])])
-
-#transformer_num.transform(X)
-## test values
+## check ColumnTransformer
 ct.fit_transform(Xtrain)
 ct.transform(Xtest)
 
 ## Step 2: model specification
-## need to specify case weight, currently treated as any other feature
 glm = PoissonRegressor()
 
 ## specify pipeline
@@ -154,19 +134,31 @@ cv = KFold(n_splits=4, shuffle=True, random_state=1)
 
 ## according to the documentation this is the percentage of deviance explained
 #cross_val_score(glm, X=Xtrain, y=ytrain, cv=cv)
+cross_val_score(pipe, X=Xtrain, y=ytrain, cv=cv, 
+                fit_params={'glm__sample_weight': swtrain})
+
 cross_val_score(pipe, X=Xtrain, y=ytrain, cv=cv)
 
 ## Step 4: fit final model
 #glm.fit(Xtrain, ytrain)
-pipe.fit(Xtrain, ytrain)
+pipe.fit(Xtrain, ytrain, **{'glm__sample_weight': swtrain})
+
+## check against vanilla glm object
+#glm.fit(ct.fit_transform(Xtrain), ytrain, sample_weight=swtrain)
+#glm.predict(ct.fit_transform(Xtrain))
 
 ## Step 5: eval final model on testing data
 ## according to the documentation this is the percentage of deviance explained
 #glm.score(Xtest, ytest)
-pipe.score(Xtest, ytest)
+pipe.score(Xtest, ytest, sample_weight=swtest)
+
+## make predictions on training data and testing data
+## neither take into account weights
+pipe.predict(Xtrain)
+pipe.predict(Xtest)
 
 ## scrap code for hist
-import matplotlib.pyplot as plt
-fig, axs = plt.subplots(1, sharey=True, tight_layout=True)
-axs.hist(df['count'], bins=20)
+#import matplotlib.pyplot as plt
+#fig, axs = plt.subplots(1, sharey=True, tight_layout=True)
+#axs.hist(df['count'], bins=20)
 ## end scrap code
